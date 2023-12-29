fac3ed2016fce003e2cb3b456ac349e07dca1d6d
Michal Pluta <vsdc48@durham.ac.uk>
2023-12-29 14:27:04 +0000

    Add helper functions

--- vector.c
+++ vector.c
@@ -34,3 +34,38 @@ void printVector(const Vector *vector, int dimension) {
     }
     printf("\n");
 }
+
+Vector* addVectors(const Vector *vector1, const Vector *vector2, const int dimension) {
+    Vector* result = mallocVector(dimension);
+    if (result == NULL) {
+        return NULL;
+    }
+    for (int i = 0; i < dimension; i++) {
+        result->values[i] = vector1->values[i] + vector2->values[i];
+    }
+    return result;
+}
+
+Vector* subVectors(const Vector *vector1, const Vector *vector2, const int dimension) {
+    Vector* result = mallocVector(dimension);
+    if (result == NULL) {
+        return NULL;
+    }
+    for (int i = 0; i < dimension; i++) {
+        result->values[i] = vector1->values[i] - vector2->values[i];
+    }
+    return result;
+}
+
+double inner_product(const Vector *vector1, const Vector *vector2, const int dimension) {
+    double result = 0;
+    for (int i = 0; i < dimension; i++) {
+        result += vector1->values[i] * vector2->values[i];
+    }
+    return result;
+}
+
+double norm(const Vector *vector, const int dimension) {
+    double innerProd = inner_product(vector, vector, dimension);
+    return sqrt(innerProd);
+}
--- vector.h
+++ vector.h
@@ -13,4 +13,11 @@ Vector* mallocVector(int dimension);
 void freeVector(Vector *vector);
 void printVector(const Vector *vector, int dimension);
 
+Vector* addVectors(const Vector *vector1, const Vector *vector2, const int dimension);
+Vector* subVectors(const Vector *vector1, const Vector *vector2, const int dimension);
+double inner_product(const Vector *vector1, const Vector *vector2, const int dimension);
+double norm(const Vector *vector, const int dimension);
+
+
+
 #endif // VECTOR_H
