43e44b50d60d4d6643dc5352f86d3d6c01b870cf
Michal Pluta <vsdc48@durham.ac.uk>
2023-12-31 21:09:26 +0000

    Remove old functions from header file

--- src/vector.h
+++ src/vector.h
@@ -9,8 +9,6 @@ Vector* mallocVector(const int dim);
 void freeVector(Vector *v);
 void printVector(const Vector *v, const int dim);
 
-Vector* addVectors(const Vector *v1, const Vector *v2, const int dim);
-Vector* subVectors(const Vector *v1, const Vector *v2, const int dim);
 double inner_product(const Vector *v1, const Vector *v2, const int dim);
 double norm(const Vector *v, const int dim);
 
