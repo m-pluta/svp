d89fa37a9e733bf49a01a3cab0f60d7febd62288
Michal Pluta <vsdc48@durham.ac.uk>
2024-01-03 01:14:00 +0000

    Reorganise test generation

--- test-gen-all.py
+++ test-gen-all.py
@@ -2,32 +2,40 @@ import subprocess
 import random
 import os
 
-try:
-    os.remove('test-gen.csv')
-    print(f"File {'test-gen.svp'} has been removed.")
-except FileNotFoundError:
-    print(f"No action taken. The file {'test-gen.svp'} does not exist.")
-
-tests = []
-
-for dimension in range(1, 51):
-    for bit_level in (8, 16, 32):
-        for _ in range(100 if dimension <= 10 else 10):
-            tests.append((str(random.getrandbits(32)), 'u', str(dimension), str(bit_level)))
-
-for dimension in range(2, 51):
-    for bit_level in (8, 16, 32):
-        for _ in range(100 if dimension <= 10 else 10):
-            tests.append((str(random.getrandbits(32)), 'r', str(dimension), str(bit_level)))
-
-
-print(len(tests))
-assert len(tests) == len(set(tests)), "Duplicate tests were generated."
-random.shuffle(tests)
-bash_script = 'test-gen.sh'
-
-for t in tests:
+def reset_output_csv():
     try:
-        subprocess.run(['bash', bash_script, t[0], t[1], t[2], t[3]], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
+        os.remove('test-gen.csv')
+        print(f"File {'test-gen.csv'} has been removed.")
+    except FileNotFoundError:
+        print(f"No action taken. The file {'test-gen.svp'} does not exist.")
+
+def generate_tests():
+    tests = []
+
+    for dimension in range(1, 51):
+        for bit_level in (8, 16, 32):
+            for _ in range(100 if dimension <= 10 else 10):
+                tests.append((str(random.getrandbits(32)), 'u', str(dimension), str(bit_level)))
+
+    for dimension in range(2, 51):
+        for bit_level in (8, 16, 32):
+            for _ in range(100 if dimension <= 10 else 10):
+                tests.append((str(random.getrandbits(32)), 'r', str(dimension), str(bit_level)))
+
+    return tests
+
+
+if __name__ == '__main__':
+    tests = generate_tests()
+
+    assert len(tests) == len(set(tests)), "Duplicate tests were generated."
+
+    random.shuffle(tests)
+
+    bash_script = 'test-gen.sh'
+
+    for t in tests:
+        try:
+            subprocess.run(['bash', bash_script, t[0], t[1], t[2], t[3]], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e}")
