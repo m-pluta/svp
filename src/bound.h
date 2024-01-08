d91b3ace6007f4c33032d7c2ed13dba4d58008f5
Michal Pluta <vsdc48@durham.ac.uk>
2024-01-08 15:21:34 +0000

    Remove old implementation

--- src/bound.h
+++
@@ -1,6 +0,0 @@
-#ifndef BOUND_H
-#define BOUND_H
-
-double lambda_1_squared(const Vector2D *Bs, const int dim);
-
-#endif // BOUND_H
--- src/lll.h
+++
@@ -1,9 +0,0 @@
-#ifndef LLL_H
-#define LLL_H
-
-#include "vector2d.h"
-#include "gram_schmidt.h"
-
-void LLL(Vector2D *B, GS_Info *gs_info, const double delta, const int dim);
-
-#endif // LLL_H
